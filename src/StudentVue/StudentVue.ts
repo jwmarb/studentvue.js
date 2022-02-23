234ca18ad295f96149689432d403ba7f92786522
EGGaming <dieptuber@gmail.com>
2022-02-23 08:28:38 -0700

    Wrapped in `try catch` block

--- src/StudentVue/StudentVue.ts
+++ src/StudentVue/StudentVue.ts
@@ -11,24 +11,28 @@ export default class StudentVue {
   }
 
   public static findDistricts(zipCode: string): Promise<SchoolDistrict[]> {
-    return new Promise(async (res) => {
-      const xmlObject: DistrictListXMLObject = await soap.Client.processAnonymousRequest(
-        'https://support.edupoint.com/Service/HDInfoCommunication.asmx',
-        {
-          paramStr: {
-            Key: '5E4B7859-B805-474B-A833-FDB15D205D40',
-            MatchToDistrictZipCode: zipCode,
-          },
-        }
-      );
-      res(
-        xmlObject.DistrictLists.DistrictInfos.DistrictInfo.map((district) => ({
-          parentVueUrl: district['@_PvueURL'],
-          address: district['@_Address'],
-          id: district['@_DistrictID'],
-          name: district['@_Name'],
-        }))
-      );
+    return new Promise(async (res, reject) => {
+      try {
+        const xmlObject: DistrictListXMLObject = await soap.Client.processAnonymousRequest(
+          'https://support.edupoint.com/Service/HDInfoCommunication.asmx',
+          {
+            paramStr: {
+              Key: '5E4B7859-B805-474B-A833-FDB15D205D40',
+              MatchToDistrictZipCode: zipCode,
+            },
+          }
+        );
+        res(
+          xmlObject.DistrictLists.DistrictInfos.DistrictInfo.map((district) => ({
+            parentVueUrl: district['@_PvueURL'],
+            address: district['@_Address'],
+            id: district['@_DistrictID'],
+            name: district['@_Name'],
+          }))
+        );
+      } catch (e) {
+        reject(e);
+      }
     });
   }
 }
