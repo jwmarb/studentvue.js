93097eb20b8167ed4eef10fbab13d408c8d4a8b6
specify_ <dieptuber@gmail.com>
2022-02-24 14:17:43 -0700

    Added `AttachmentXMLObject` to define parsed xml

---
+++ src/StudentVue/Attachment/Attachment.xml.d.ts
@@ -0,0 +1,10 @@
+export declare interface AttachmentXMLObject {
+  AttachmentXML: [
+    {
+      '@_DocumentName': [string];
+      'xmlns:xsd': [string];
+      'xmlns:xsi': [string];
+      Base64Code: [string];
+    }
+  ];
+}
