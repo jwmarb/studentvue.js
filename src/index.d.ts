3a1808cd31e502cf64abbeb022f0a2ed8c90a38a
specify_ <dieptuber@gmail.com>
2022-02-24 14:17:00 -0700

    Exported types to be available in package

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,4 @@ export * from './StudentVue/StudentVue';
 export * from './StudentVue/Client/Client';
 export * from './StudentVue/Client/Client.interfaces';
 export * from './utils/soap/Client/Client.interfaces';
+export * from './utils/types';
