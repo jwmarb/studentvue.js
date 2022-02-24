d5a8a8bfa115faa49764d5637a5f05bb7ee91cfb
specify_ <dieptuber@gmail.com>
2022-02-24 13:01:47 -0700

    Fixed constructor to accept non-array parsed xml

--- src/StudentVue/RequestException/RequestException.ts
+++ src/StudentVue/RequestException/RequestException.ts
@@ -1,4 +1,4 @@
-import { ParsedRequestError } from '../../utils/soap/Client/Client.interfaces';
+import { ParsedAnonymousRequestError, ParsedRequestError } from '../../utils/soap/Client/Client.interfaces';
 
 export default class RequestException {
   private _message: string;
@@ -12,10 +12,13 @@ export default class RequestException {
     return this._stackTrace;
   }
 
-  public constructor(obj: ParsedRequestError | { message: string; stack?: string }) {
+  public constructor(obj: ParsedRequestError | { message: string; stack?: string } | ParsedAnonymousRequestError) {
     if ('message' in obj) {
       this._message = obj.message;
       this._stackTrace = obj.stack;
+    } else if (Array.isArray(obj.RT_ERROR)) {
+      this._message = obj.RT_ERROR[0]['@_ERROR_MESSAGE'][0];
+      this._stackTrace = obj.RT_ERROR[0].STACK_TRACE[0];
     } else {
       this._message = obj.RT_ERROR['@_ERROR_MESSAGE'];
       this._stackTrace = obj.RT_ERROR.STACK_TRACE;
