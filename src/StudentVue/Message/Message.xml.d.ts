9deaa5fdcdcbdcd141d9735b5ec9d375825cda65
specify_ <dieptuber@gmail.com>
2022-02-24 17:41:22 -0700

    Fixed improper type definition

--- src/StudentVue/Message/Message.xml.d.ts
+++ src/StudentVue/Message/Message.xml.d.ts
@@ -1,30 +1,34 @@
 export declare interface MessageXMLObject {
-  MessageListings: [
+  PXPMessagesData: [
     {
-      MessageListing: {
-        '@_IconURL': string;
-        '@_ID': string;
-        '@_BeginDate': string;
-        '@_Type': string;
-        '@_Subject': string;
-        '@_Content': string; // This is a long html in a string
-        '@_Read': string;
-        '@_Deletable': string;
-        '@_From': string;
-        '@_SubjectNoHTML': string;
-        '@_Module': string;
-        '@_Email': string;
-        '@_StaffGU': string;
-        '@_SMMsgPersonGU': string;
-        AttachmentDatas: [
-          {
-            AttachmentData: {
-              '@_AttachmentName': string;
-              '@_SmAttachmentGU': string;
-            }[];
-          }
-        ];
-      }[];
+      MessageListings: [
+        {
+          MessageListing: {
+            '@_IconURL': string;
+            '@_ID': string;
+            '@_BeginDate': string;
+            '@_Type': string;
+            '@_Subject': string;
+            '@_Content': string; // This is a long html in a string
+            '@_Read': string;
+            '@_Deletable': string;
+            '@_From': string;
+            '@_SubjectNoHTML': string;
+            '@_Module': string;
+            '@_Email': string;
+            '@_StaffGU': string;
+            '@_SMMsgPersonGU': string;
+            AttachmentDatas: [
+              {
+                AttachmentData: {
+                  '@_AttachmentName': string;
+                  '@_SmAttachmentGU': string;
+                }[];
+              }
+            ];
+          }[];
+        }
+      ];
     }
   ];
 }
