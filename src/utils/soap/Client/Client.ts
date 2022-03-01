3171c689ad97ca502a319e892be464721f169cd0
EGGaming <dieptuber@gmail.com>
2022-03-01 15:06:54 -0700

    Added documentation for future maintainers

--- src/utils/soap/Client/Client.ts
+++ src/utils/soap/Client/Client.ts
@@ -40,6 +40,36 @@ export default class Client {
     this.__district__ = credentials.districtUrl;
   }
 
+  /**
+   * Create a POST request to synergy servers to fetch data
+   * @param options Options to provide when making a XML request to the servers
+   * @returns Returns an XML object that must be defined in a type declaration file.
+   * @link See https://github.com/StudentVue/docs
+   * @example
+   * ```js
+   * super.processRequest({ methodName: 'Refer to StudentVue/docs', paramStr: { AnythingThatCanBePassed: true, AsLongAsItMatchesTheDocumentation: true }});
+   * // This will make the XML request below:
+   * ```
+   * 
+   * ```xml
+   * <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>
+        <ProcessWebServiceRequest xmlns="http://edupoint.com/webservices/">
+            <userID>STUDENT_USERNAME</userID>
+            <password>STUDENT_PASSWORD</password>
+            <skipLoginLog>1</skipLoginLog>
+            <parent>0</parent>
+            <webServiceHandleName>PXPWebServices</webServiceHandleName>
+            <methodName>Refer to StudentVue/docs</methodName>
+            <paramStr>
+              <AnythingThatCanBePassed>true</AnythingThatCanBePassed>
+              <AsLongAsItMatchesTheDocumentation>true</AsLongAsItMatchesTheDocumentation>
+            </paramStr>
+        </ProcessWebServiceRequest>
+    </soap:Body>
+</soap:Envelope>
+   * ```
+   */
   protected processRequest<T>(options: RequestOptions): Promise<T> {
     const defaultOptions: RequestOptions = {
       skipLoginLog: 1,
