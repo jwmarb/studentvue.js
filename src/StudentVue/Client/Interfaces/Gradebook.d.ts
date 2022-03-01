7fc59d3a6732c6b22604d3e42c7b4103d92e02b9
EGGaming <dieptuber@gmail.com>
2022-03-01 10:00:23 -0700

    Defiend gradebook types

---
+++ src/StudentVue/Client/Interfaces/Gradebook.d.ts
@@ -0,0 +1,115 @@
+import ResourceType from '../../../Constants/ResourceType';
+
+export declare interface Gradebook {
+  error: string;
+  type: string;
+  reportingPeriod: {
+    current: ReportingPeriod;
+    available: ReportingPeriod[];
+  };
+  courses: Course[];
+}
+
+export declare interface ReportingPeriod {
+  index: number;
+  name: string;
+  date: {
+    start: Date;
+    end: Date;
+  };
+}
+
+export declare interface Course {
+  period: number;
+  title: string;
+  room: string;
+  staff: {
+    name: string;
+    email: string;
+    staffGu: string;
+  };
+  marks: Mark[];
+}
+
+export declare interface Mark {
+  name: string;
+  calculatedScore: {
+    string: string;
+    raw: number;
+  };
+  weightedCategories: WeightedCategory[];
+  assignments: Assignment[];
+}
+
+export declare interface WeightedCategory {
+  calculatedMark: string;
+  type: string;
+  weight: {
+    evaluated: string;
+    standard: string;
+  };
+  points: {
+    current: number;
+    possible: number;
+  };
+}
+
+export declare interface Assignment {
+  gradebookId: string;
+  name: string;
+  type: string;
+  date: {
+    start: Date;
+    due: Date;
+  };
+  score: {
+    type: string;
+    value: string;
+  };
+  points: string;
+  notes: string;
+  teacherId: string;
+  description: string;
+  hasDropbox: boolean;
+  studentId: string;
+  dropboxDate: {
+    start: Date;
+    end: Date;
+  };
+  resources: (FileResource | URLResource)[];
+}
+
+export declare interface FileResource extends Resource {
+  file: {
+    type: string;
+    name: string;
+    uri: string;
+  };
+  resource: {
+    date: Date;
+    id: string;
+    name: string;
+  };
+  type: ResourceType.FILE;
+}
+
+export declare interface URLResource extends Resource {
+  url: string;
+  type: ResourceType.URL;
+  resource: {
+    date: Date;
+    id: string;
+    name: string;
+    description: string;
+  };
+  path: string;
+}
+
+export declare interface Resource {
+  classId: string;
+
+  gradebookId: string;
+
+  sequence: string;
+  teacherId: string;
+}
---
+++ src/StudentVue/Client/Interfaces/xml/Gradebook.d.ts
@@ -0,0 +1,121 @@
+export declare interface GradebookXMLObject {
+  Gradebook: [
+    {
+      '@_Type': [string];
+      '@_ErrorMessage': [string];
+      '@_HideStandardGraphInd': [string];
+      '@_HideMarksColumnElementary': [string];
+      '@_HidePointsColumnElementary': [string];
+      '@_HidePercentSecondary': [string];
+      '@_DisplayStandardsData': [string];
+      '@_GBStandardsTabDefault': [string];
+      ReportingPeriods: [
+        {
+          ReportPeriod: {
+            '@_Index': [string];
+            '@_GradePeriod': [string];
+            '@_StartDate': [string];
+            '@_EndDate': [string];
+          }[];
+        }
+      ];
+      ReportingPeriod: [{ '@_GradePeriod': [string]; '@_StartDate': [string]; '@_EndDate': [string] }];
+      Courses: {
+        Course: [
+          {
+            '@_UsesRichContent': [string];
+            '@_Period': [string];
+            '@_Title': [string];
+            '@_Room': [string];
+            '@_Staff': [string];
+            '@_StaffEMail': [string];
+            '@_StaffGU': [string];
+            '@_HighlightPercentageCutOffForProgressBar': [string];
+            Marks: {
+              Mark: [
+                {
+                  '@_MarkName': [string];
+                  '@_CalculatedScoreString': [string];
+                  '@_CalculatedScoreRaw': [string];
+                  StandardViews: [''];
+                  GradeCalculationSummary:
+                    | [
+                        {
+                          AssignmentGradeCalc: [
+                            {
+                              '@_Type': [string];
+                              '@_Weight': [string];
+                              '@_Points': [string];
+                              '@_PointsPossible': [string];
+                              '@_WeightedPct': [string];
+                              '@_CalculatedMark': [string];
+                            }
+                          ];
+                        }
+                      ]
+                    | [''];
+                  Assignments: [
+                    {
+                      Assignment: {
+                        '@_GradebookID': [string];
+                        '@_Measure': [string];
+                        '@_Type': [string];
+                        '@_Date': [string];
+                        '@_DueDate': [string];
+                        '@_Score': [string];
+                        '@_ScoreType': [string];
+                        '@_Points': [string];
+                        '@_Notes': [string];
+                        '@_TeacherID': [string];
+                        '@_StudentID': [string];
+                        '@_MeasureDescription': [string];
+                        '@_HasDropBox': [string];
+                        '@_DropStartDate': [string];
+                        '@_DropEndDate': [string];
+                        Resources:
+                          | {
+                              Resource: [URLResourceXMLObject] | [FileResourceXMLObject];
+                            }[]
+                          | [''];
+                        Standards: [''];
+                      }[];
+                    }
+                  ];
+                }
+              ];
+            }[];
+          }
+        ];
+      }[];
+    }
+  ];
+}
+
+export declare type URLResourceXMLObject = {
+  '@_ClassID': [string];
+  '@_GradebookID': [string];
+  '@_ResourceDate': [string];
+  '@_ResourceDescription': [string];
+  '@_ResourceID': [string];
+  '@_ResourceName': [string];
+  '@_Sequence': [string];
+  '@_TeacherID': [string];
+  '@_Type': ['URL'];
+  '@_URL': [string];
+  '@_ServerFileName': [string];
+};
+
+export declare type FileResourceXMLObject = {
+  '@_ClassID': [string];
+  '@_FileName': [string];
+  '@_FileType': [string];
+  '@_GradebookID': [string];
+  '@_ResourceDate': [string];
+  '@_ResourceID': [string];
+  '@_ResourceName': [string];
+  '@_Sequence': [string];
+  '@_TeacherID': [string];
+  '@_Type': ['File'];
+  '@_URL': [string];
+  '@_ServerFileName': [string];
+};
