511ab423ce7f47d39df7e6fc526c9eb116261a62
EGGaming <dieptuber@gmail.com>
2022-03-02 13:23:42 -0700

    Added documentation

--- src/StudentVue/Icon/Icon.ts
+++ src/StudentVue/Icon/Icon.ts
@@ -1,6 +1,12 @@
 export default class Icon {
   public readonly uri: string;
   public constructor(path: string, hostUrl: string) {
+    /**
+     * The URI of the icon
+     * @public
+     * @readonly
+     * @type {string}
+     */
     this.uri = hostUrl + path;
   }
 }
