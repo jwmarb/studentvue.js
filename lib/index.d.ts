b2d0322a0e504db1a6e4c2c995796620a320758b
EGGaming <dieptuber@gmail.com>
2022-03-04 12:08:06 -0700

    compiled

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,6 +9,7 @@ declare module 'studentvue' {
     export { default as Icon } from 'studentvue/StudentVue/Icon/Icon';
     export { default as Client } from 'studentvue/StudentVue/Client/Client';
     export { default as Attachment } from 'studentvue/StudentVue/Attachment/Attachment';
+    export { default as File } from 'studentvue/StudentVue/File/File';
     export * from 'studentvue/StudentVue/ReportCard';
     export * from 'studentvue/StudentVue/Document';
     export * from 'studentvue/StudentVue/Client/Client.interfaces';
@@ -275,6 +276,38 @@ declare module 'studentvue/StudentVue/Attachment/Attachment' {
           * ```
           */
         get(): Promise<Base64String>;
+    }
+}
+
+declare module 'studentvue/StudentVue/File/File' {
+    import { LoginCredentials } from 'studentvue/utils/soap/Client/Client.interfaces';
+    import soap from 'studentvue/utils/soap/soap';
+    export default abstract class File<T> extends soap.Client {
+            readonly documentGu: string;
+            constructor(credentials: LoginCredentials, documentGu: string, methodName: string);
+            /**
+                * Parse the XML object to translate it into an ordinary object. This method must be written for every class that extends Document (which gets the file from synergy servers using a POST fetch request)
+                * @param {unknown} xmlObject The XML Object passed after parsing
+                * @protected
+                * @returns {T} Returns a reformatted XML object to make it easier for code
+                * @example
+                * ```js
+                * const xmlObject = await super.processRequest({...}); // { "@_Attribute": [{ "@_Nested": [{...}, {...}]}]}
+                * parseXMLObject(xmlObject); // { attribute: { nested: [{...}, {...}] } }
+                *
+                * ```
+                */
+            protected abstract parseXMLObject(xmlObject: unknown): T;
+            /**
+                * Retrieve the file from synergy servers. After retrieving the xmlObject, this method calls parseXMLObject which must be defined to parse the xmlObject into a readable, typesafe object.
+                * @public
+                * @returns {Promise<T>} Returns a base64 object
+                * @example
+                * ```js
+                * const base64 = await document.get(); // { attribute: { nested: {...} }, base64: "base64 code" }
+                * ```
+                */
+            get(): Promise<T>;
     }
 }
 
@@ -873,38 +906,6 @@ declare module 'studentvue/utils/soap/Client/Client' {
           */
         protected processRequest<T>(options: RequestOptions): Promise<T>;
         static processAnonymousRequest<T>(url: string, options?: Partial<RequestOptions>): Promise<T>;
-    }
-}
-
-declare module 'studentvue/StudentVue/File/File' {
-    import { LoginCredentials } from 'studentvue/utils/soap/Client/Client.interfaces';
-    import soap from 'studentvue/utils/soap/soap';
-    export default abstract class File<T> extends soap.Client {
-            readonly documentGu: string;
-            constructor(credentials: LoginCredentials, documentGu: string, methodName: string);
-            /**
-                * Parse the XML object to translate it into an ordinary object. This method must be written for every class that extends Document (which gets the file from synergy servers using a POST fetch request)
-                * @param {unknown} xmlObject The XML Object passed after parsing
-                * @protected
-                * @returns {T} Returns a reformatted XML object to make it easier for code
-                * @example
-                * ```js
-                * const xmlObject = await super.processRequest({...}); // { "@_Attribute": [{ "@_Nested": [{...}, {...}]}]}
-                * parseXMLObject(xmlObject); // { attribute: { nested: [{...}, {...}] } }
-                *
-                * ```
-                */
-            protected abstract parseXMLObject(xmlObject: unknown): T;
-            /**
-                * Retrieve the file from synergy servers. After retrieving the xmlObject, this method calls parseXMLObject which must be defined to parse the xmlObject into a readable, typesafe object.
-                * @public
-                * @returns {Promise<T>} Returns a base64 object
-                * @example
-                * ```js
-                * const base64 = await document.get(); // { attribute: { nested: {...} }, base64: "base64 code" }
-                * ```
-                */
-            get(): Promise<T>;
     }
 }
 
