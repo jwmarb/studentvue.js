f5ce152180bec6fd0acb83523b764286a37c54d9
specify_ <dieptuber@gmail.com>
2022-03-04 17:29:25 -0700

    Refactored `RequestException` to be more readable

--- src/StudentVue/RequestException/RequestException.ts
+++ src/StudentVue/RequestException/RequestException.ts
@@ -1,27 +1,22 @@
 import { ParsedAnonymousRequestError, ParsedRequestError } from '../../utils/soap/Client/Client.interfaces';
 
+/**
+ * RequestException is a class used to parse errors from Synergy servers
+ */
 export default class RequestException {
-  private _message: string;
-  private _stackTrace: string | undefined;
-
-  public get message() {
-    return this._message;
-  }
-
-  public get stack() {
-    return this._stackTrace;
-  }
+  public readonly message: string;
+  public readonly stack: string | undefined;
 
   public constructor(obj: ParsedRequestError | { message: string; stack?: string } | ParsedAnonymousRequestError) {
     if ('message' in obj) {
-      this._message = obj.message;
-      this._stackTrace = obj.stack;
+      this.message = obj.message;
+      this.stack = obj.stack;
     } else if (Array.isArray(obj.RT_ERROR)) {
-      this._message = obj.RT_ERROR[0]['@_ERROR_MESSAGE'][0];
-      this._stackTrace = obj.RT_ERROR[0].STACK_TRACE[0];
+      this.message = obj.RT_ERROR[0]['@_ERROR_MESSAGE'][0];
+      this.stack = obj.RT_ERROR[0].STACK_TRACE[0];
     } else {
-      this._message = obj.RT_ERROR['@_ERROR_MESSAGE'];
-      this._stackTrace = obj.RT_ERROR.STACK_TRACE;
+      this.message = obj.RT_ERROR['@_ERROR_MESSAGE'];
+      this.stack = obj.RT_ERROR.STACK_TRACE;
     }
   }
 }
