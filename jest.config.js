7b1ef14ebd7a039e11ede68d92dc4faa35acbc3b
specify_ <dieptuber@gmail.com>
2022-08-08 17:44:47 -0700

    add jest-extended for more jest functionality

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['jest-extended/all'],
   modulePaths: ['<rootDir>/src/'],
   silent: false,
   verbose: true,
